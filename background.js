4d02c1a6f01216ad6aef0ef2212a1a644614d6da
matthewdias <matthewdias@me.com>
2016-05-25 19:47:34 -0500

    context menu, open in current or adjacent tabs

--- background.js
+++ background.js
@@ -1,22 +1,57 @@
 //Methods
 function newtab(method) {
-	chrome.tabs.query({ currentWindow: true, active: true }, callback);
+	chrome.tabs.query({ currentWindow: true, active: true }, function(tab) {
+		chrome.tabs.create(closerTab(tab));
+	});
 }
 
-function callback(tabs) {
-	console.log("tabs: " + tabs);
-	parent = tabs[0];
-	console.log("parent: " + parent.id);
-	console.log("index: " + parent.index + 1);
+function closerTab(tabs, url) {
+	tab = tabs[0];
+	// console.log("id: " + tab.id);
+	// console.log("index: " + tab.index + 1);
 
-	props = {
-		index: parent.index + 1,
-		openerTabId: parent.id
+	return {
+		index: tab.index + 1,
+		openerTabId: tab.id,
+		url: url
 	}
-	chrome.tabs.create(props);
+}
+
+function closerOpen(info, tabs) {
+	chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+		chrome.tabs.create(closerTab(tabs, info.linkUrl));
+	});
+}
+
+function openHere(info, tabs) {
+	chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+		chrome.tabs.update(tabs[0].id, { url: info.linkUrl });
+	});
 }
 
 chrome.commands.onCommand.addListener(function(command) {
     if(command == 'newtab')
     	newtab();
 });
+
+chrome.contextMenus.create({
+		"id": "closeropen",
+		"title": "Open Link in Adjacent New Tab",
+		"contexts": ['link'],
+		"onclick": closerOpen
+	},
+	function() {
+		// console.log('context created')
+	}
+);
+
+chrome.contextMenus.create({
+		"id": "openhere",
+		"title": "Open Link in Current Tab",
+		"contexts": ['link'],
+		"onclick": openHere
+	},
+	function() {
+		// console.log('context created')
+	}
+);
