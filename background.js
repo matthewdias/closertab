89daf00a4690821fab60f9e58246638d823d6923
matthewdias <matthewdias@me.com>
2016-05-25 22:27:01 -0500

    select multiple tabs with keyboard, pop selected tabs to new window

--- background.js
+++ background.js
@@ -1,4 +1,26 @@
 //Methods
+function highlight(dir) {
+	chrome.tabs.query({ currentWindow: true, highlighted: true }, function(tabs) {
+		var highlights = [];
+		var target;
+		if(dir === 'left') {
+			target = tabs[0].index - 1
+		}
+		else {
+			target = tabs[Object.keys(tabs)[Object.keys(tabs).length - 1]].index + 1
+		}
+		highlights.push(target)
+
+		for(key in tabs) {
+			tab = tabs[key]
+			highlights.push(tab.index)
+			// console.log(highlights)
+		}
+		// console.log(highlights)
+		chrome.tabs.highlight({tabs: highlights})
+	});
+}
+
 function newtab(method) {
 	chrome.tabs.query({ currentWindow: true, active: true }, function(tab) {
 		chrome.tabs.create(closerTab(tab));
@@ -29,9 +51,32 @@ function openHere(info, tabs) {
 	});
 }
 
+function popTabs() {
+	var pops = [];
+	chrome.tabs.query({ currentWindow: true, highlighted: true }, function(tabs) {
+		for(key in tabs) {
+			tab = tabs[key]
+			pops.push(tab.id)
+		}
+		console.log(pops)
+	});
+	chrome.windows.create({}, function (window) {
+		chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+			chrome.tabs.remove(tabs[0].id);
+		});
+		chrome.tabs.move(pops, { windowId: window.id, index: 0 });
+	});
+}
+
 chrome.commands.onCommand.addListener(function(command) {
+	if(command == 'highlight-left')
+    	highlight('left');
+    if(command == 'highlight-right')
+    	highlight('right');
     if(command == 'newtab')
     	newtab();
+    if(command == 'poptabs')
+    	popTabs();
 });
 
 chrome.contextMenus.create({
